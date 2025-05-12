56565e07c8ced591c3392085fbfb5acebe5922f7
yawneazy <yawneazy>
2025-05-12 16:49:13 -0700

    Added some nav links

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import homeImage from './images/home-image.jpg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import homeImage from './images/home-image.jpg';
+import './App.css';
+import { About } from './pages/About';
+import { Music } from './pages/Music';
 
 function App() {
   return (
-    <>
-    <img src={homeImage} alt="Home" className="full-screen-image"></img>
-
-    </>
-  )
+    <Router>
+      <img src={homeImage} alt="Home" className="full-screen-image" />
+      {/* COMING SOON */}
+      <Routes> 
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/music" element={<Music />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Music.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Music() {
+  return <h1>About Page</h1>;
+}
