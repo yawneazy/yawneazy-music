181b33620c0abeb184af10c00876a520e0155b64
yawneazy <yawneazy>
2025-07-01 08:50:47 -0700

    Saving changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import homeImage from './images/home-image.jpg';
 import './App.css';
 import { About } from './pages/About';
@@ -7,12 +7,22 @@ import { Music } from './pages/Music';
 function App() {
   return (
     <Router>
-      <img src={homeImage} alt="Home" className="full-screen-image" />
-      {/* COMING SOON */}
-      <div className="yawneazy-text">YAWNEAZY</div>
-      <div className="coming-soon-msg">Coming soon...</div>
-      <Routes> 
-        <Route path="/" element={<div>Home</div>} />
+      <div className="hero-container">
+        <img src={homeImage} alt="Home" className="full-screen-image" />
+
+        {/* Overlaid content */}
+        <div className="overlay-content">
+          <div className="yawneazy-text">YAWNEAZY</div>
+          <div className="nav-links">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/music">Music</Link>
+          </div>
+        </div>
+      </div>
+
+      <Routes>
+        {/* <Route path="/" element={<div>Home</div>} /> */}
         <Route path="/about" element={<About />} />
         <Route path="/music" element={<Music />} />
       </Routes>
